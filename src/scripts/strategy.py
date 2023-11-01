ce99fdda818e50023a096039fa89777302f88082
rubgarcia97 <rubengarciaolivas@gmail.com>
2023-11-01 23:14:19 +0100

    strategy_1 real time

--- src/scripts/strategy.py
+++ src/scripts/strategy.py
@@ -1,95 +1,86 @@
 from binance import Pynance
+from datetime import datetime
 
 import pandas as pd
 import numpy as np
 import json
+import time
 
 
-def strategy_1(wallet):
+def strategy_1(symbol:str,wallet:int, lags:int, interval:int, SMA_periods:int, save:bool):
+
 
     """
     En esta primera estrategia, definiremos signals de compra y de venta.
 
     El criterio a seguir sera el de: P > MA[n] para buy signals y P<MA[n] para sell signals
 
-    
+    :param symbol:required
+    :param wallet: Importe inicial de la cartera que se utilizara para la primera operacion
+    :param lags: iteraciones del bucle
+    :param interval: segundos que dejaremos pasar entre iteracion
+    :param SMA_periods: n periodos de calculo del SMA
+    :param save: Guardar los resultados en .csv
     """
-
-    Pynance().market_data(symbol="BTCUSDT",lags=1200,interval=1)
-
-    with open("../results/BTCUSDT_price.json",'r') as f:
-        data = json.loads(f.read())
-    data = pd.json_normalize(data,record_path=['BTCUSDT'])
-    data = data.set_index('timestamp')
-
-    #Transform variables
-    data["price"] = data["price"].astype(float)
-
-
-    #Add columns
-    data["MA_5"] = data.price.rolling(5).mean()
-    data["MA_25"] = data.price.rolling(25).mean()
-    data["MA_50"] = data.price.rolling(50).mean()
-    
-    data["signal"] = np.where(
-        (data['price'].notna()) & (data['MA_25'].notna()),
-        np.where(data['price'] > data['MA_25'], 'buy', 'sell'),
-        np.nan  # En caso de NaN, establece 'signal' como NaN
-    )
-
-    #Save dataframe
-    data.to_csv("../results/strategy_1/strategy_1_raw.csv", index=True)
-
-
-    # ============================== STRATEGY ==============================
-
-    results = []
 
     wallet = wallet
     prev_signal = None
 
-    try:
+    MA = f"MA_{SMA_periods}"
 
-        for row in range(len(data)):
-            
-            if data.iloc[row, 4] == "buy" and prev_signal != "buy":
-                wallet = np.divide(wallet,data.iloc[row,0])
-                prev_signal = data.iloc[row, 4]
+    data = pd.DataFrame(columns=['price',MA,"signal"])
+    results = pd.DataFrame(columns=['wallet'])
 
-                results.append({"index": data.index[row], "wallet": wallet * data.iloc[row,0]})
+    for lag in range(lags):
+        result = Pynance().market_data(symbol=symbol,lags=1,interval=interval,save=False)
 
-            elif data.iloc[row, 4] == "sell" and prev_signal not in ["sell",None]:
-                wallet = np.multiply(wallet,data.iloc[row,0])
-                prev_signal = data.iloc[row, 4]
+        symbol_data = result[symbol][0]
+        price = float(symbol_data["price"])
+        timestamp = pd.to_datetime(symbol_data["timestamp"])
 
-                results.append({"index": data.index[row], "wallet": wallet})
+        data.loc[timestamp] = price
 
-            else:
-                continue
+        
+        data[MA] = data.price.rolling(SMA_periods).mean()
+        data["signal"] = np.where(
+            (data['price'].notna()) & (data[MA].notna()),
+            np.where(data['price'] > data[MA], 'buy', 'sell'),
+            np.nan  # En caso de NaN, establece 'signal' como NaN
+        )
 
-        df = pd.DataFrame(results)
-        df['index'] = pd.to_datetime(df['index'])
-        df.set_index('index', inplace=True)
+        if data.loc[timestamp,'signal'] == "buy" and prev_signal != "buy":
+            wallet = np.divide(wallet,data.loc[timestamp,"price"])
+            prev_signal = data.loc[timestamp,'signal']
 
-    except KeyError:
-        print("No se ha dado ninguna 'buy signal'")
-    
+            results.loc[timestamp] = wallet * data.loc[timestamp,"price"]
+
+
+        elif data.loc[timestamp,'signal'] == "sell" and prev_signal not in ["sell",None]:
+            wallet = np.multiply(wallet,data.loc[timestamp,"price"])
+            prev_signal = data.loc[timestamp,'signal']
+
+            results.loc[timestamp] = wallet
+
+        else:
+            continue
+
     #calculate profitability
-    
-    t0 = df.iloc[0,0]
-    t1 = df.iloc[-1,0]
+    t0 = results.iloc[0,0]
+    t1 = results.iloc[-1,0]
 
     rentabilidad = (t1 - t0)/t0
     print(format(rentabilidad, ".2%"))
+        
 
-    #save results
-
-    with open("../results/strategy_1/strategy_1_results.csv", 'w') as csv_file:
-        df.to_csv(csv_file, index=False, header=True)
-
+    if save:
+        data.to_csv("../results/strategy_1/strategy_1_raw.csv", index=True)
+        results.to_csv("../results/strategy_1/strategy_1_results.csv", index=True)
+    else:
+        pass
 
 
 
 
 if __name__=="__main__":
-    strategy_1(wallet=100)
+
+    strategy_1(symbol="BTCUSDT",wallet=100,lags=900,interval=1,SMA_periods=25,save=True)
